cfadb750111c36992c98f31ea852edbe69377cd5
Amestest <gktk8411@naver.com>
2024-06-10 18:04:03 +0900

    servlet

---
+++ demoProject1/src/main/java/com/example/demo/DemoProject1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProject1Application.class, args);
+	}
+
+}
+
---
+++ demoProject1/src/main/resources/static/js/ex.js
@@ -0,0 +1,9 @@
+/**
+ * javascript 코드를 작성한느 공간
+ * 
+ * jqeury
+ */
+
+ document.querySelector("#btn").addEventListerner("click",()=>{
+	alert("클림됨");
+ })
